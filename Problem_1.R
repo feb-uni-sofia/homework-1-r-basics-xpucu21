abb88ea5f7e772b6046873d425952a18cf59bf0d
Hristina Trifonova <hristina.t21@gmail.com>
2018-04-02 15:15:13 +0300

    Homework 1 - Problem 1, Problem 2

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,23 @@
-
+#Problem 1
+#a)
+x<-c(4,1,1,4)
+#b)
+y<-c(1,4)
+#c)
+x-y
+#The two vectors are with different size. The second vector "y" is smaller and it becomes (1,4,1,4)
+#d)
+s<-c(x,y)
+s
+#e)
+rep(s,10)
+ss<-rep(s,10)
+length(ss)
+#f)
+rep(s,each=3)
+#g)
+seq(from=7,to=21,by=1)
+7:21
+#h)
+dd<-seq(7,21,by=1)
+length(dd) 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,25 @@
-
+#Problem 2
+#a)
+xmin<-c(23,20.5,28.2,20.3,22.4,17.2,18.2)
+xmax<-c(25,22.8,31.2,27.3,28.4,20.2,24.1)
+#b)
+diff<-xmax-xmin
+#c)
+avgMin<-mean(xmin)
+avgMax<-mean(xmax)
+#d)
+xmin[xmin<avgMin]
+#e)
+xmin[xmax>avgMax]
+#f)
+names(xmin)<-c('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+names(xmax)<-c('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+#g)
+temperature<-data.frame(xmin,xmax)
+#h)
+temperature<-within(temperature,{Fahrenheit<-9/5*xmin+32})
+#i)
+Fahrenheitnew<-data.frame(fxmin=9/5*xmin+32,fxmax=9/5*xmax+32)
+#j)
+Fahfivedays<-data.frame(fxmin=xmin[seq(1:5)]*9/5+32,fxmax=xmax[seq(1:5)]*9/5+32)
+Fahfivedayss<-data.frame(fxmin=xmin[seq(length(xmin)-2)]*9/5+32,fxmax=xmax[seq(length(xmax)-2)]*9/5+32) 
